c3172cb658b78dd9598149e72a4c72a7966bc592
shdopeoplesn <shdopeoplesn@gmail.com>
2020-05-19 15:05:29 +0800

    first commit

---
+++ kill_process_when_idle/kill_process_when_idle/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace kill_process_when_idle
+{
+    partial class kill_process_when_idle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(kill_process_when_idle));
+            this.lbl_program_name = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbl_remain_time = new System.Windows.Forms.Label();
+            this.lbl_current_program_name = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_program_name
+            // 
+            this.lbl_program_name.AutoSize = true;
+            this.lbl_program_name.Location = new System.Drawing.Point(4, 9);
+            this.lbl_program_name.Name = "lbl_program_name";
+            this.lbl_program_name.Size = new System.Drawing.Size(99, 13);
+            this.lbl_program_name.TabIndex = 0;
+            this.lbl_program_name.Text = "PROGRAM_NAME";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbl_remain_time
+            // 
+            this.lbl_remain_time.AutoSize = true;
+            this.lbl_remain_time.Location = new System.Drawing.Point(131, 81);
+            this.lbl_remain_time.Name = "lbl_remain_time";
+            this.lbl_remain_time.Size = new System.Drawing.Size(81, 13);
+            this.lbl_remain_time.TabIndex = 1;
+            this.lbl_remain_time.Text = "REMAIN_TIME";
+            // 
+            // lbl_current_program_name
+            // 
+            this.lbl_current_program_name.AutoSize = true;
+            this.lbl_current_program_name.Location = new System.Drawing.Point(131, 46);
+            this.lbl_current_program_name.Name = "lbl_current_program_name";
+            this.lbl_current_program_name.Size = new System.Drawing.Size(158, 13);
+            this.lbl_current_program_name.TabIndex = 2;
+            this.lbl_current_program_name.Text = "CURRENT_PROGRAM_NAME";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "目前使用的程式:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ERP關閉剩餘時間:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(167, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "秒";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(300, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "ver. 1.0";
+            // 
+            // kill_process_when_idle
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(365, 118);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_current_program_name);
+            this.Controls.Add(this.lbl_remain_time);
+            this.Controls.Add(this.lbl_program_name);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "kill_process_when_idle";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.kill_process_when_idle_Shown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_program_name;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbl_remain_time;
+        private System.Windows.Forms.Label lbl_current_program_name;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ kill_process_when_idle/kill_process_when_idle/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace kill_process_when_idle
+{
+    public partial class kill_process_when_idle : Form
+    {
+        //參數設定區
+        const String PROGRAM_NAME = "ERP閒置中斷程式";
+        const String KILL_PROCESS_NAME = "erp2017m";
+        const String KILL_PROCESS_KEYWORD = "ERP2017";
+        int MAX_REMAIN_TIME = 30;
+        int REMAIN_TIME = 30;
+
+
+        //抓取運行程式標題的DLL引入
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern int GetWindowTextLength(IntPtr hWnd);
+
+        private string GetCaptionOfActiveWindow()
+        {
+            var strTitle = string.Empty;
+            var handle = GetForegroundWindow();
+            // Obtain the length of the text   
+            var intLength = GetWindowTextLength(handle) + 1;
+            var stringBuilder = new StringBuilder(intLength);
+            if (GetWindowText(handle, stringBuilder, intLength) > 0)
+            {
+                strTitle = stringBuilder.ToString();
+            }
+            return strTitle;
+        }
+
+        public void StartCount() {
+            if (timer1.Enabled == false) {
+                REMAIN_TIME = MAX_REMAIN_TIME;
+                timer1.Start();
+            }
+        }
+        public void StopCount()
+        {
+            if (timer1.Enabled == true)
+            {
+                timer1.Stop();
+                lbl_remain_time.Text = MAX_REMAIN_TIME.ToString();
+            }
+        }
+
+        public void KillProgram() {
+            foreach (var process in Process.GetProcessesByName(KILL_PROCESS_NAME))
+            {
+                process.Kill();
+            }
+        }
+
+        public void InitUI()
+        {
+            this.notifyIcon1.Visible = false;
+
+            this.Text = PROGRAM_NAME;
+            lbl_program_name.Text = PROGRAM_NAME;
+        }
+
+        public kill_process_when_idle()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitUI();
+
+            //避免多開
+            if (Process.GetProcessesByName("kill_process_when_idle").Count() > 1)
+            {
+                MessageBox.Show("此程式不能多開!");
+                Process.GetCurrentProcess().Kill();
+            }
+        }
+        private void kill_process_when_idle_Shown(object sender, EventArgs e)
+        {
+            this.notifyIcon1.Visible = true;
+            this.Hide();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.notifyIcon1.Visible = true;
+                this.Hide();
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.notifyIcon1.Visible = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (REMAIN_TIME <= 0) {
+                KillProgram();
+                timer1.Stop();
+            }
+            lbl_remain_time.Text = REMAIN_TIME.ToString();
+            REMAIN_TIME--;
+        }
+
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            lbl_current_program_name.Text = GetCaptionOfActiveWindow();
+            if (Process.GetProcessesByName(KILL_PROCESS_NAME).Count() > 0)
+            {
+                if (lbl_current_program_name.Text.Contains(KILL_PROCESS_KEYWORD))
+                {
+                    StopCount();
+                }
+                else
+                {
+                    StartCount();
+                }
+            }
+            else {
+                StopCount();
+            }
+        }
+    }
+}
